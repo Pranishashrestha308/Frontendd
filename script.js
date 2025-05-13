efde90d8ddfe69d874a4d376e0bcbc755d1b0cea
Pranisha <cresthapranisha27@gmail.com>
2025-05-13 07:36:43 +0545

    make projects

--- script.js
+++ script.js
@@ -4,14 +4,68 @@
 // Kalinchwok.addEventListener('click',()=>{navlinks.classList.toggle('active')});
 
 
-const text = "Welcome to Kalinchwok Resort";
-const typedtext = document.getElementById('typed-text');
-let index = 0;
-function type(){
-    if (index < text.length){
-        typedtext.textContent += text.charAt(index);
-        index++;
-        setTimeout(type,150);
-}
-}
-type();
+// const text = "Welcome to Kalinchwok Resort";
+// const typedtext = document.getElementById('typed-text');
+// let index = 0;
+// function type(){
+//     if (index < text.length){
+//         typedtext.textContent += text.charAt(index);
+//         index++;
+//         setTimeout(type,150);
+// }
+// }
+// type();
+// projects
+const projects = [
+{
+    image:"kuriii.jpg",
+    title : "Kuri village",
+    description : "Kuri village, located in the Dolakha district of Nepal."
+},
+{
+    image : "gaurishankar.jpg",
+    title : " Gaurishankar Himal",
+    description : "Gaurishankar Himal, located in the Dolakha district of Nepal."
+},
+{
+    image : "tshorolpa.jpg",
+    title : "Tsho Rolpa Lake",
+    description : "Tsho Rolpa Lake, situated at an elevation of approximately 4,580 meters."
+},
+]
+const container = document.getElementById("project");
+projects.forEach(project=>{
+    const item = document.createElement("div");
+    item.className = "project-item";
+    item.innerHTML = `
+    <img src = "${project.image}"alt="${project.title}"/>
+    <h3>${project.title}</h3>
+    <p>${ project.description}</p>
+    `;
+    container.appendChild(item);
+});
+const town=[
+    {
+        image : "kuriii.jpg",
+        title : "Kuri village",
+        description : "Kuri village, located in the Dolakha district of Nepal."
+    },
+    {
+        image : "gaurishankar.jpg",
+        title : "Gaurishankar Himal",
+        description : "Gaurishankar Himal, located in the Dolakha district of Nepal."
+    },
+]
+const conatiners = document.getElementById("town");
+towns.forEach(town=>{
+    const item = document.createElement("div");
+    item.className = "towns";
+    item.innerHTML = `
+    <img src ="${towns.image}"alt="${towns.title}"/>
+    <h3>${towns.title}</h3>
+    <p>${towns.description}</p>
+    `;
+    containers .appendChild(item);
+});
+    
+   
